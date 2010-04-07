c001e4a546ee96e3f5a5600b5aebc5c66d992a31
Gerald Evans <USER@DOMAIN>
2010-04-07 23:15:13 +0000

    Internal data structure changed and added Enabled flag
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@33 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

--- SwapScreen/SwapScreen/KeyCombo.cs
+++ SwapScreen/SwapScreen/KeyCombo.cs
@@ -32,31 +32,101 @@ namespace SwapScreen
 	/// </summary>
 	public struct KeyCombo
 	{
+		// The value of the KeyCombo is held in an uint,
+		// where the least 16 bits are used to hold the key code,
+		// the bitmask 0x0100 indicates if Alt is pressed,
+		// 0x0200 indicates if Control is pressed,
+		// 0x0400 indicates if Shift is pressed,
+		// 0x0800 indicates if Win is pressed,
+		// and we have a flag at 0x8000 which if set indicates that 
+		// the KeyCombo is disabled
+		private const uint FLAG_ALT = 0x0100;		// Win32.MOD_ALT << 16
+		private const uint FLAG_CONTROL = 0x0200;	// Win32.MOD_CONTROL << 16
+		private const uint FLAG_SHIFT = 0x0400;		// Win32.MOD_SHIFT << 16
+		private const uint FLAG_WIN = 0x0800;		// Win32.MOD_WIN << 16
+
+		// Note we set the bit to indicate disbaled (rather than enabled)
+		// to maintain backward compatibility with previous versions of this
+		// program which didn't have the disable ability
+		private const uint FLAG_DISABLED = 0x8000;
+
+
+		private uint comboValue;
+		public uint ComboValue
+		{
+			get { return comboValue; }
+			set { comboValue = value; }
+		}
+
+		public bool Enabled
+		{
+			get { return !IsSet(comboValue, FLAG_DISABLED); }
+			set { SetBit(!value, FLAG_DISABLED); }
+		}
+
 		/// <summary>
 		/// The virtual keycode (excluding any modifier)
 		/// only the lesat sig 16 bits of this will be used
 		/// </summary>
-		public Keys KeyCode;
+		public Keys KeyCode
+		{
+			//get { return (Keys)(comboValue & 0xFFFF); }
+			get { return (Keys)(comboValue & 0x00FF); }
+			set { comboValue = (comboValue & 0xFF00) | ((uint)value & 0x00FF); }
+		}
 
 		/// <summary>
 		/// Flag indicating if the Alt key is pressed.
 		/// </summary>
-		public bool AltMod;
+		public bool AltMod
+		{
+			get { return IsSet(comboValue, FLAG_ALT); }
+			set { SetBit(value, FLAG_ALT); }
+		}
 
 		/// <summary>
 		/// Flag indicating if the Shift key is pressed.
 		/// </summary>
-		public bool ShiftMod;
+		public bool ShiftMod
+		{
+			get { return IsSet(comboValue, FLAG_SHIFT); }
+			set { SetBit(value, FLAG_SHIFT); }
+		}
 
 		/// <summary>
 		/// Flag indicating if the Control key is pressed.
 		/// </summary>
-		public bool ControlMod;
+		public bool ControlMod
+		{
+			get { return IsSet(comboValue, FLAG_CONTROL); }
+			set { SetBit(value, FLAG_CONTROL); }
+		}
 
 		/// <summary>
 		/// Flag indicating if the Windows key is pressed.
 		/// </summary>
-		public bool WinMod;
+		public bool WinMod
+		{
+			get { return IsSet(comboValue, FLAG_WIN); }
+			set { SetBit(value, FLAG_WIN); }
+		}
+
+		private bool IsSet(uint value, uint flag)
+		{
+			return (value & flag) != 0;
+		}
+
+		private void SetBit(bool set, uint flag)
+		{
+			if (set)
+			{
+				comboValue |= flag;
+			}
+			else
+			{
+				comboValue &= ~flag;
+			}
+		}
 
 		/// <summary>
 		/// Returns the KeyCode as used by Win32.
@@ -103,19 +173,21 @@ namespace SwapScreen
 			}
 		}
 
-		/// <summary>
-		/// Ctor to to specify a key combo that can be used as a hotkey
-		/// </summary>
-		/// <param name="keyCode">Vitual key code</param>
-		/// <param name="win32Modifier">Alt/Ctrl/Shift/Win modifier as used by Win32</param>
-		public KeyCombo(Keys keyCode, uint win32Modifier)
-		{
-			KeyCode = keyCode;
-			AltMod = (win32Modifier & Win32.MOD_ALT) != 0;
-			ControlMod = (win32Modifier & Win32.MOD_CONTROL) != 0;
-			ShiftMod = (win32Modifier & Win32.MOD_SHIFT) != 0;
-			WinMod = (win32Modifier & Win32.MOD_WIN) != 0;
-		}
+		///// <summary>
+		///// Ctor to to specify a key combo that can be used as a hotkey
+		///// </summary>
+		///// <param name="keyCode">Vitual key code</param>
+		///// <param name="win32Modifier">Alt/Ctrl/Shift/Win modifier as used by Win32</param>
+		//public KeyCombo(bool enabled, Keys keyCode, uint win32Modifier)
+		//{
+		//    comboValue = 0;
+		//    Enabled = enabled;
+		//    KeyCode = keyCode;
+		//    AltMod = (win32Modifier & Win32.MOD_ALT) != 0;
+		//    ControlMod = (win32Modifier & Win32.MOD_CONTROL) != 0;
+		//    ShiftMod = (win32Modifier & Win32.MOD_SHIFT) != 0;
+		//    WinMod = (win32Modifier & Win32.MOD_WIN) != 0;
+		//}
 
 		/// <summary>
 		/// Converts the state of the KeyCombo to a single uint
@@ -124,7 +196,7 @@ namespace SwapScreen
 		/// <returns>uint that can be passed to FromPropertyValue() to restore the current state.</returns>
 		public uint ToPropertyValue()
 		{
-			return Win32KeyCode | (Win32Modifier << 16);
+			return comboValue;
 		}
 
 		/// <summary>
@@ -133,12 +205,7 @@ namespace SwapScreen
 		/// <param name="config">uint value from a previous call to ToPropertyValue().</param>
 		public void FromPropertyValue(uint config)
 		{
-			KeyCode = (Keys)(config & 0xFFFF);
-			uint keyModifier = config >> 16;
-			AltMod = (keyModifier & Win32.MOD_ALT) != 0;
-			ControlMod = (keyModifier & Win32.MOD_CONTROL) != 0;
-			ShiftMod = (keyModifier & Win32.MOD_SHIFT) != 0;
-			WinMod = (keyModifier & Win32.MOD_WIN) != 0;
+			comboValue = config;
 		}
 	}
 }
