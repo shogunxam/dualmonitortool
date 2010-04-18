2f7e60d8d0979c585408f1e5cab748376409f34c
Gerald Evans <USER@DOMAIN>
2010-04-18 11:14:12 +0000

    Version changed to 1.2
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@56 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

--- SwapScreen/SwapScreen/Properties/AssemblyInfo.cs
+++ SwapScreen/SwapScreen/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GNE")]
 [assembly: AssemblyProduct("SwapScreen")]
-[assembly: AssemblyCopyright("Copyright © 2009 Gerald Evans")]
+[assembly: AssemblyCopyright("Copyright © 2010 Gerald Evans")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
