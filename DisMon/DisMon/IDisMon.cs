5cffdf41e5efe7b731f91d66a8ee866103660259
Gerald Evans <USER@DOMAIN>
2010-07-08 10:14:30 +0000

    i/f required by windows version specific implementation
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@133 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

---
+++ DisMon/DisMon/IDisMon.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DisMon
+{
+	interface IDisMon
+	{
+		//public void Init();
+
+		int Count();
+		void Revert();
+		void MarkAsPrimary(int newPrimaryIndex);
+		void MarkAllSecondaryAsDisabled();
+		bool IsDisabled(int monitorIndex);
+		void MarkAsDisabled(int monitorIndex);
+		void MarkAsEnabled(int enableIndex);
+		bool ApplyChanges();
+		void Restore();
+	}
+}
