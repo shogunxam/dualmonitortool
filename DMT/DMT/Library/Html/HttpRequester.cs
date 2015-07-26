c01a7029aaf15a538ae66dfda90cb6acf277d181
Gerald Evans <USER@DOMAIN>
2015-07-26 14:44:04 +0000

    DMT: Partial update (continued)
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@382 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

--- DMT/DMT/Library/Html/HttpRequester.cs
+++ DMT/DMT/Library/Html/HttpRequester.cs
@@ -35,12 +35,25 @@ namespace DMT.Library.Html
 	{
 		public Uri LastResponseUri { get; protected set; }
 
-		public string GetPage(HttpConnection connection, Uri uri, string testPage)
+		IHttpConnectionManager _connectionManager;
+
+		const string UserAgent = "Dual Monitor Tools/2.1";
+
+		// max redirects for a single request, before we give up
+		const int MAX_REDIRECTS = 5;
+
+		public HttpRequester(IHttpConnectionManager connectionManager)
+		{
+			_connectionManager = connectionManager;
+		}
+
+		public string GetPage(Uri uri, string testPage, out HttpConnection repliedConnection)
 		{
 			string pageData = string.Empty;
 
-			HttpWebRequest webRequest = connection.CreateGetRequest(uri);
-			HttpResponseData responseData = connection.GetResponse(webRequest);
+			HttpConnection connection = _connectionManager.GetConnection(uri);
+			HttpWebRequest webRequest = CreateGetRequest(connection, uri);
+			HttpResponseData responseData = GetResponse(connection, webRequest, out repliedConnection);
 			if (responseData.StatusCode == HttpStatusCode.OK)
 			{
 				pageData = responseData.EncodeAsString();
@@ -50,12 +63,14 @@ namespace DMT.Library.Html
 			return pageData;
 		}
 
-		public Image GetImage(HttpConnection connection, Uri uri)
+		public Image GetImage(Uri uri)
 		{
 			Image image = null;
 
-			HttpWebRequest webRequest = connection.CreateGetRequest(uri);
-			HttpResponseData responseData = connection.GetResponse(webRequest);
+			HttpConnection connection = _connectionManager.GetConnection(uri);
+			HttpWebRequest webRequest = CreateGetRequest(connection, uri);
+			HttpConnection repliedConnection;
+			HttpResponseData responseData = GetResponse(connection, webRequest, out repliedConnection);
 #if DEBUG
 			SaveImageToDisk(responseData);
 #endif
@@ -67,6 +82,212 @@ namespace DMT.Library.Html
 
 			return image;
 		}
+
+		#region Request creation
+
+		HttpWebRequest CreateGetRequest(HttpConnection connection, Uri uri)
+		{
+			HttpWebRequest webRequest = CreateRequest(connection, uri);
+			webRequest.Method = "GET";
+
+			return webRequest;
+		}
+
+		HttpWebRequest CreatePostRequest(HttpConnection connection, Uri uri, List<Tuple<string, string>> postParameters)
+		{
+			HttpWebRequest webRequest = CreateRequest(connection, uri);
+			webRequest.Method = "POST";
+			webRequest.ContentType = "application/x-www-form-urlencoded";
+
+			// we will handle the redirection
+			webRequest.AllowAutoRedirect = false;
+
+			// encode the post parameters
+			Encoding urlEncoding = Encoding.Default;
+			string postParameterAsString = EncodePostParameters(postParameters, urlEncoding);
+			byte[] postParamtersAsBytes = Encoding.ASCII.GetBytes(postParameterAsString);
+
+			// add the encoded post parameters
+			webRequest.ContentLength = postParamtersAsBytes.Length;
+			Stream requestStream = webRequest.GetRequestStream();
+			requestStream.Write(postParamtersAsBytes, 0, postParamtersAsBytes.Length);
+			requestStream.Close();
+
+			return webRequest;
+		}
+
+		HttpWebRequest CreateRequest(HttpConnection connection, Uri uri)
+		{
+			HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(uri);
+			if (connection.LastUri != null)
+			{
+				webRequest.Referer = connection.LastUri.AbsolutePath;
+			}
+
+			webRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+			webRequest.Headers.Add("Accept-Language", "en-us,en;q=0.5");
+			webRequest.Headers.Add("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+			webRequest.UserAgent = UserAgent;
+
+			CookieContainer cookieContainer = new CookieContainer();
+			cookieContainer.Add(connection.Cookies);
+			webRequest.CookieContainer = cookieContainer;
+
+
+			return webRequest;
+		}
+
+		string EncodePostParameters(List<Tuple<string, string>> postParameters, Encoding urlEncoding)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			foreach (Tuple<string, string> postParameter in postParameters)
+			{
+				if (sb.Length > 0)
+				{
+					sb.Append("&");
+				}
+				//sb.Append(HttpUtility.UrlEncode(postParameter.Item1, urlEncoding));
+				sb.Append(HttpHelper.UrlEncode(postParameter.Item1, urlEncoding));
+				if (postParameter.Item2 != null)
+				{
+					sb.Append("=");
+					//sb.Append(HttpUtility.UrlEncode(postParameter.Item2, urlEncoding));
+					sb.Append(HttpHelper.UrlEncode(postParameter.Item2, urlEncoding));
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		#endregion
+
+		#region Response handling
+		HttpResponseData GetResponse(HttpConnection connection, HttpWebRequest webRequest, out HttpConnection repliedConnection)
+		{
+			HttpResponseData responseData = null;
+			HttpConnection curConnection = connection;
+			HttpWebResponse webResponse = null;
+
+			//responseData = connection.GetResponse(webRequest);
+			webResponse = CatchGetResponse(webRequest);
+
+			int redirectNum = 0;
+			if (IsARedirect(webResponse.StatusCode) && redirectNum < MAX_REDIRECTS)
+			{
+				redirectNum++;
+
+				// save the cookies
+				curConnection.MergeCookies(webResponse.Cookies);
+
+				// save the uri of the page that actually reponded to us
+				curConnection.LastUri = webRequest.Address;
+
+
+				// create a new request to the redirected page
+				Uri uri = GetFullUri(webResponse.GetResponseHeader("Location"), curConnection);
+				// this may be on another host, so make sure we have the correct connection
+				curConnection = _connectionManager.GetConnection(uri);
+				HttpWebRequest redirectedWebRequest = CreateGetRequest(curConnection, uri);
+
+				// close existing response
+				webResponse.Close();
+
+				// now start new request
+				webResponse = CatchGetResponse(webRequest);
+			}
+
+			// save the cookies
+			curConnection.MergeCookies(webResponse.Cookies);
+
+			// save the uri of the page that actually reponded to us
+			curConnection.LastUri = webRequest.Address;
+
+			byte[] rawData = ReadResponseStream(webResponse);
+			responseData = new HttpResponseData(webResponse.StatusCode, rawData, webResponse.ContentType);
+
+			webResponse.Close();
+
+			repliedConnection = curConnection;
+			return responseData;
+		}
+
+		HttpWebResponse CatchGetResponse(HttpWebRequest webRequest)
+		{
+			try
+			{
+				return (HttpWebResponse)webRequest.GetResponse();
+			}
+			catch (WebException ex)
+			{
+				if (ex.Response != null)
+				{
+					return (HttpWebResponse)ex.Response;
+				}
+				throw;
+			}
+		}
+
+		bool IsARedirect(HttpStatusCode statusCode)
+		{
+			// for now, just support 302, 301
+			if (statusCode == HttpStatusCode.Redirect || statusCode == HttpStatusCode.MovedPermanently)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		byte[] ReadResponseStream(HttpWebResponse webResponse)
+		{
+			byte[] rawData = null;
+
+			Stream responseStream = webResponse.GetResponseStream();
+
+			int bytesRead;
+			byte[] buffer = new byte[16384];
+
+			while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) != 0)
+			{
+				int offset = 0;
+				if (rawData == null)
+				{
+					offset = 0;
+					rawData = new byte[bytesRead];
+				}
+				else
+				{
+					byte[] previousData = rawData;
+					offset = rawData.Length;
+					rawData = new byte[offset + bytesRead];
+					previousData.CopyTo(rawData, 0);
+				}
+
+				Array.Copy(buffer, 0, rawData, offset, bytesRead);
+			}
+
+			responseStream.Close();
+
+			return rawData;
+		}
+
+		Uri GetFullUri(string relativeUrl, HttpConnection parentConnection)
+		{
+			Uri uri;
+			if (parentConnection != null)
+			{
+				uri = parentConnection.ExpandPath(relativeUrl);
+			}
+			else
+			{
+				uri = new Uri(relativeUrl);
+			}
+
+			return uri;
+		}
+
+		#endregion
 
 #if DEBUG
 		void SaveImageToDisk(HttpResponseData responseData)
