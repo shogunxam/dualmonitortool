0fca4e8cacb541b2c6f67380fa6548ee300b7c91
Gerald Evans <USER@DOMAIN>
2016-01-13 12:00:33 +0000

    Installer: simple version with no gui
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@410 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

---
+++ Installer/Build.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Wix Toolset v3.10\bin\Candle" DualMonitorTools.wks -ext WixNetFxExtension
+"C:\Program Files (x86)\Wix Toolset v3.10\bin\Light" DualMonitorTools.wixobj -ext WixNetFxExtension
+
