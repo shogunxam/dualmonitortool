ebb524d521cb8e77c4abe5888760b6e92e44e146
Gerald Evans <USER@DOMAIN>
2020-09-19 17:27:41 +0000

    Removed unneeded 'Identify' button
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@477 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

--- DMT/DMT/Modules/General/GeneralMonitorOrderingOptionsPanel.Designer.cs
+++ DMT/DMT/Modules/General/GeneralMonitorOrderingOptionsPanel.Designer.cs
@@ -32,7 +32,6 @@
 			this.groupBoxOrder = new System.Windows.Forms.GroupBox();
 			this.radioButtonDotNet = new System.Windows.Forms.RadioButton();
 			this.radioButtonLeftRight = new System.Windows.Forms.RadioButton();
-			this.buttonIdentify = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
 			this.groupBoxOrder.SuspendLayout();
 			this.SuspendLayout();
@@ -81,20 +80,10 @@
 			this.radioButtonLeftRight.UseVisualStyleBackColor = true;
 			this.radioButtonLeftRight.CheckedChanged += new System.EventHandler(this.radioButtonLeftRight_CheckedChanged);
 			// 
-			// buttonIdentify
-			// 
-			this.buttonIdentify.Location = new System.Drawing.Point(354, 189);
-			this.buttonIdentify.Name = "buttonIdentify";
-			this.buttonIdentify.Size = new System.Drawing.Size(75, 23);
-			this.buttonIdentify.TabIndex = 3;
-			this.buttonIdentify.Text = "Identify";
-			this.buttonIdentify.UseVisualStyleBackColor = true;
-			// 
 			// GeneralMonitorOrderingOptionsPanel
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.buttonIdentify);
 			this.Controls.Add(this.groupBoxOrder);
 			this.Controls.Add(this.picPreview);
 			this.Name = "GeneralMonitorOrderingOptionsPanel";
@@ -112,6 +101,5 @@
 		private System.Windows.Forms.GroupBox groupBoxOrder;
 		private System.Windows.Forms.RadioButton radioButtonDotNet;
 		private System.Windows.Forms.RadioButton radioButtonLeftRight;
-		private System.Windows.Forms.Button buttonIdentify;
 	}
 }
