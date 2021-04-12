8206fd1868924893356484afc381b3025d84cfc5
Gerald Evans <gne>
2021-04-12 18:43:37 +0000

    Added dead space cursor jumping Options. Split cursor options panel into 3.
    git-svn-id: svn://svn.code.sf.net/p/dualmonitortool/code/trunk@484 fb2ba5e6-08d8-4d4c-bdde-3d14dca355d2

---
+++ DMT/DMT/Modules/Cursor/CursorDeadSpaceOptionsPanel.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DMT.Modules.Cursor
+{
+	partial class CursorDeadSpaceOptionsPanel
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.checkBoxDeadSpaceXJump = new System.Windows.Forms.CheckBox();
+			this.checkBoxDeadSpaceYJump = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(139, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Dead space cursor jumping.";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 23);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(297, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "This only applies if you have a non rectangular monitor layout.";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(3, 50);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(494, 36);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Normally, you can\'t move the cursor from a monitor if there isn\'t a monitor at th" +
+    "e target location of the cursor.";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(3, 86);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(455, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "This option allows the cursor to leave the monitor and jump to a location on the " +
+    "nearest monitor.";
+			// 
+			// checkBoxDeadSpaceXJump
+			// 
+			this.checkBoxDeadSpaceXJump.AutoSize = true;
+			this.checkBoxDeadSpaceXJump.Location = new System.Drawing.Point(6, 111);
+			this.checkBoxDeadSpaceXJump.Name = "checkBoxDeadSpaceXJump";
+			this.checkBoxDeadSpaceXJump.Size = new System.Drawing.Size(337, 17);
+			this.checkBoxDeadSpaceXJump.TabIndex = 4;
+			this.checkBoxDeadSpaceXJump.Text = "Allow cursor to jump across dead space when moving horizontally.";
+			this.checkBoxDeadSpaceXJump.UseVisualStyleBackColor = true;
+			this.checkBoxDeadSpaceXJump.CheckedChanged += new System.EventHandler(this.checkBoxDeadSpaceXJump_CheckedChanged);
+			// 
+			// checkBoxDeadSpaceYJump
+			// 
+			this.checkBoxDeadSpaceYJump.AutoSize = true;
+			this.checkBoxDeadSpaceYJump.Location = new System.Drawing.Point(6, 134);
+			this.checkBoxDeadSpaceYJump.Name = "checkBoxDeadSpaceYJump";
+			this.checkBoxDeadSpaceYJump.Size = new System.Drawing.Size(326, 17);
+			this.checkBoxDeadSpaceYJump.TabIndex = 5;
+			this.checkBoxDeadSpaceYJump.Text = "Allow cursor to jump across dead space when moving vertically.";
+			this.checkBoxDeadSpaceYJump.UseVisualStyleBackColor = true;
+			this.checkBoxDeadSpaceYJump.CheckedChanged += new System.EventHandler(this.checkBoxDeadSpaceYJump_CheckedChanged);
+			// 
+			// CursorDeadSpaceOptionsPanel
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.checkBoxDeadSpaceYJump);
+			this.Controls.Add(this.checkBoxDeadSpaceXJump);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "CursorDeadSpaceOptionsPanel";
+			this.Size = new System.Drawing.Size(500, 360);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.CheckBox checkBoxDeadSpaceXJump;
+		private System.Windows.Forms.CheckBox checkBoxDeadSpaceYJump;
+	}
+}
--- DMT/DMT/Modules/Cursor/CursorGeneralOptionsPanel.Designer.cs
+++ DMT/DMT/Modules/Cursor/CursorGeneralOptionsPanel.Designer.cs
@@ -28,92 +28,15 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.comboBoxFreeMovementKey = new System.Windows.Forms.ComboBox();
-			this.checkBoxPrimaryReturnUnhindered = new System.Windows.Forms.CheckBox();
-			this.comboBoxCursorMode = new System.Windows.Forms.ComboBox();
-			this.label15 = new System.Windows.Forms.Label();
-			this.checkBoxControlUnhindersCursor = new System.Windows.Forms.CheckBox();
-			this.groupBox4 = new System.Windows.Forms.GroupBox();
 			this.label13 = new System.Windows.Forms.Label();
 			this.label11 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
-			this.scrollBarSticky = new System.Windows.Forms.HScrollBar();
 			this.groupBox3 = new System.Windows.Forms.GroupBox();
 			this.hotKeyPanelCursorToPrimaryScreen = new DMT.Library.HotKeys.HotKeyPanel();
 			this.hotKeyPanelCursorPrevScreen = new DMT.Library.HotKeys.HotKeyPanel();
 			this.hotKeyPanelCursorNextScreen = new DMT.Library.HotKeys.HotKeyPanel();
-			this.hotKeyPanelLockCursor = new DMT.Library.HotKeys.HotKeyPanel();
-			this.hotKeyPanelStickyCursor = new DMT.Library.HotKeys.HotKeyPanel();
-			this.hotKeyPanelFreeCursor = new DMT.Library.HotKeys.HotKeyPanel();
-			this.checkBoxAllowButton = new System.Windows.Forms.CheckBox();
-			this.comboBoxFreeMovementButton = new System.Windows.Forms.ComboBox();
-			this.groupBox4.SuspendLayout();
 			this.groupBox3.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// comboBoxFreeMovementKey
-			// 
-			this.comboBoxFreeMovementKey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBoxFreeMovementKey.FormattingEnabled = true;
-			this.comboBoxFreeMovementKey.Location = new System.Drawing.Point(279, 251);
-			this.comboBoxFreeMovementKey.Name = "comboBoxFreeMovementKey";
-			this.comboBoxFreeMovementKey.Size = new System.Drawing.Size(194, 21);
-			this.comboBoxFreeMovementKey.TabIndex = 52;
-			this.comboBoxFreeMovementKey.SelectedIndexChanged += new System.EventHandler(this.comboBoxFreeMovementKey_SelectedIndexChanged);
-			// 
-			// checkBoxPrimaryReturnUnhindered
-			// 
-			this.checkBoxPrimaryReturnUnhindered.AutoSize = true;
-			this.checkBoxPrimaryReturnUnhindered.Location = new System.Drawing.Point(10, 301);
-			this.checkBoxPrimaryReturnUnhindered.Name = "checkBoxPrimaryReturnUnhindered";
-			this.checkBoxPrimaryReturnUnhindered.Size = new System.Drawing.Size(254, 17);
-			this.checkBoxPrimaryReturnUnhindered.TabIndex = 51;
-			this.checkBoxPrimaryReturnUnhindered.Text = "Allow cursor to return freely to the primary screen";
-			this.checkBoxPrimaryReturnUnhindered.UseVisualStyleBackColor = true;
-			this.checkBoxPrimaryReturnUnhindered.CheckedChanged += new System.EventHandler(this.checkBoxPrimaryReturnUnhindered_CheckedChanged);
-			// 
-			// comboBoxCursorMode
-			// 
-			this.comboBoxCursorMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBoxCursorMode.FormattingEnabled = true;
-			this.comboBoxCursorMode.Location = new System.Drawing.Point(174, 318);
-			this.comboBoxCursorMode.Name = "comboBoxCursorMode";
-			this.comboBoxCursorMode.Size = new System.Drawing.Size(300, 21);
-			this.comboBoxCursorMode.TabIndex = 50;
-			this.comboBoxCursorMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxCursorMode_SelectedIndexChanged);
-			// 
-			// label15
-			// 
-			this.label15.AutoSize = true;
-			this.label15.Location = new System.Drawing.Point(10, 321);
-			this.label15.Name = "label15";
-			this.label15.Size = new System.Drawing.Size(155, 13);
-			this.label15.TabIndex = 49;
-			this.label15.Text = "Default cursor mode on startup:";
-			// 
-			// checkBoxControlUnhindersCursor
-			// 
-			this.checkBoxControlUnhindersCursor.AutoSize = true;
-			this.checkBoxControlUnhindersCursor.Location = new System.Drawing.Point(11, 255);
-			this.checkBoxControlUnhindersCursor.Name = "checkBoxControlUnhindersCursor";
-			this.checkBoxControlUnhindersCursor.Size = new System.Drawing.Size(249, 17);
-			this.checkBoxControlUnhindersCursor.TabIndex = 48;
-			this.checkBoxControlUnhindersCursor.Text = "Allow cursor to move freely if this key is pressed";
-			this.checkBoxControlUnhindersCursor.UseVisualStyleBackColor = true;
-			this.checkBoxControlUnhindersCursor.CheckedChanged += new System.EventHandler(this.checkBoxControlUnhindersCursor_CheckedChanged);
-			// 
-			// groupBox4
-			// 
-			this.groupBox4.Controls.Add(this.label13);
-			this.groupBox4.Controls.Add(this.label11);
-			this.groupBox4.Controls.Add(this.label5);
-			this.groupBox4.Controls.Add(this.scrollBarSticky);
-			this.groupBox4.Location = new System.Drawing.Point(3, 206);
-			this.groupBox4.Name = "groupBox4";
-			this.groupBox4.Size = new System.Drawing.Size(477, 43);
-			this.groupBox4.TabIndex = 47;
-			this.groupBox4.TabStop = false;
-			this.groupBox4.Text = "Sticky cursor options";
 			// 
 			// label13
 			// 
@@ -142,36 +65,22 @@
 			this.label5.TabIndex = 39;
 			this.label5.Text = "Resistance to movement between screens";
 			// 
-			// scrollBarSticky
-			// 
-			this.scrollBarSticky.Location = new System.Drawing.Point(264, 14);
-			this.scrollBarSticky.Maximum = 3000;
-			this.scrollBarSticky.Name = "scrollBarSticky";
-			this.scrollBarSticky.Size = new System.Drawing.Size(177, 17);
-			this.scrollBarSticky.SmallChange = 10;
-			this.scrollBarSticky.TabIndex = 38;
-			this.scrollBarSticky.Value = 100;
-			this.scrollBarSticky.ValueChanged += new System.EventHandler(this.scrollBarSticky_ValueChanged);
-			// 
 			// groupBox3
 			// 
 			this.groupBox3.Controls.Add(this.hotKeyPanelCursorToPrimaryScreen);
 			this.groupBox3.Controls.Add(this.hotKeyPanelCursorPrevScreen);
 			this.groupBox3.Controls.Add(this.hotKeyPanelCursorNextScreen);
-			this.groupBox3.Controls.Add(this.hotKeyPanelLockCursor);
-			this.groupBox3.Controls.Add(this.hotKeyPanelStickyCursor);
-			this.groupBox3.Controls.Add(this.hotKeyPanelFreeCursor);
 			this.groupBox3.Location = new System.Drawing.Point(3, 3);
 			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(477, 194);
+			this.groupBox3.Size = new System.Drawing.Size(477, 112);
 			this.groupBox3.TabIndex = 46;
 			this.groupBox3.TabStop = false;
-			this.groupBox3.Text = "Cursor movement HotKeys";
+			this.groupBox3.Text = "Cursor HotKeys";
 			// 
 			// hotKeyPanelCursorToPrimaryScreen
 			// 
 			this.hotKeyPanelCursorToPrimaryScreen.Description = "Description";
-			this.hotKeyPanelCursorToPrimaryScreen.Location = new System.Drawing.Point(8, 164);
+			this.hotKeyPanelCursorToPrimaryScreen.Location = new System.Drawing.Point(8, 79);
 			this.hotKeyPanelCursorToPrimaryScreen.Name = "hotKeyPanelCursorToPrimaryScreen";
 			this.hotKeyPanelCursorToPrimaryScreen.Size = new System.Drawing.Size(465, 23);
 			this.hotKeyPanelCursorToPrimaryScreen.TabIndex = 5;
@@ -179,7 +88,7 @@
 			// hotKeyPanelCursorPrevScreen
 			// 
 			this.hotKeyPanelCursorPrevScreen.Description = "Description";
-			this.hotKeyPanelCursorPrevScreen.Location = new System.Drawing.Point(8, 135);
+			this.hotKeyPanelCursorPrevScreen.Location = new System.Drawing.Point(8, 50);
 			this.hotKeyPanelCursorPrevScreen.Name = "hotKeyPanelCursorPrevScreen";
 			this.hotKeyPanelCursorPrevScreen.Size = new System.Drawing.Size(465, 23);
 			this.hotKeyPanelCursorPrevScreen.TabIndex = 4;
@@ -187,99 +96,31 @@
 			// hotKeyPanelCursorNextScreen
 			// 
 			this.hotKeyPanelCursorNextScreen.Description = "Description";
-			this.hotKeyPanelCursorNextScreen.Location = new System.Drawing.Point(8, 106);
+			this.hotKeyPanelCursorNextScreen.Location = new System.Drawing.Point(8, 21);
 			this.hotKeyPanelCursorNextScreen.Name = "hotKeyPanelCursorNextScreen";
 			this.hotKeyPanelCursorNextScreen.Size = new System.Drawing.Size(465, 23);
 			this.hotKeyPanelCursorNextScreen.TabIndex = 3;
-			// 
-			// hotKeyPanelLockCursor
-			// 
-			this.hotKeyPanelLockCursor.Description = "Description";
-			this.hotKeyPanelLockCursor.Location = new System.Drawing.Point(8, 77);
-			this.hotKeyPanelLockCursor.Name = "hotKeyPanelLockCursor";
-			this.hotKeyPanelLockCursor.Size = new System.Drawing.Size(465, 23);
-			this.hotKeyPanelLockCursor.TabIndex = 2;
-			// 
-			// hotKeyPanelStickyCursor
-			// 
-			this.hotKeyPanelStickyCursor.Description = "Description";
-			this.hotKeyPanelStickyCursor.Location = new System.Drawing.Point(8, 48);
-			this.hotKeyPanelStickyCursor.Name = "hotKeyPanelStickyCursor";
-			this.hotKeyPanelStickyCursor.Size = new System.Drawing.Size(465, 23);
-			this.hotKeyPanelStickyCursor.TabIndex = 1;
-			// 
-			// hotKeyPanelFreeCursor
-			// 
-			this.hotKeyPanelFreeCursor.Description = "Description";
-			this.hotKeyPanelFreeCursor.Location = new System.Drawing.Point(8, 19);
-			this.hotKeyPanelFreeCursor.Name = "hotKeyPanelFreeCursor";
-			this.hotKeyPanelFreeCursor.Size = new System.Drawing.Size(465, 23);
-			this.hotKeyPanelFreeCursor.TabIndex = 0;
-			// 
-			// checkBoxAllowButton
-			// 
-			this.checkBoxAllowButton.AutoSize = true;
-			this.checkBoxAllowButton.Location = new System.Drawing.Point(11, 278);
-			this.checkBoxAllowButton.Name = "checkBoxAllowButton";
-			this.checkBoxAllowButton.Size = new System.Drawing.Size(262, 17);
-			this.checkBoxAllowButton.TabIndex = 53;
-			this.checkBoxAllowButton.Text = "Allow cursor to move freely if this button is pressed";
-			this.checkBoxAllowButton.UseVisualStyleBackColor = true;
-			this.checkBoxAllowButton.CheckedChanged += new System.EventHandler(this.checkBoxAllowButton_CheckedChanged);
-			// 
-			// comboBoxFreeMovementButton
-			// 
-			this.comboBoxFreeMovementButton.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBoxFreeMovementButton.FormattingEnabled = true;
-			this.comboBoxFreeMovementButton.Location = new System.Drawing.Point(279, 276);
-			this.comboBoxFreeMovementButton.Name = "comboBoxFreeMovementButton";
-			this.comboBoxFreeMovementButton.Size = new System.Drawing.Size(194, 21);
-			this.comboBoxFreeMovementButton.TabIndex = 54;
-			this.comboBoxFreeMovementButton.SelectedIndexChanged += new System.EventHandler(this.comboBoxFreeMovementButton_SelectedIndexChanged);
 			// 
 			// CursorGeneralOptionsPanel
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.comboBoxFreeMovementButton);
-			this.Controls.Add(this.checkBoxAllowButton);
-			this.Controls.Add(this.comboBoxFreeMovementKey);
-			this.Controls.Add(this.checkBoxPrimaryReturnUnhindered);
-			this.Controls.Add(this.comboBoxCursorMode);
-			this.Controls.Add(this.label15);
-			this.Controls.Add(this.checkBoxControlUnhindersCursor);
-			this.Controls.Add(this.groupBox4);
 			this.Controls.Add(this.groupBox3);
 			this.Name = "CursorGeneralOptionsPanel";
 			this.Size = new System.Drawing.Size(500, 360);
-			this.groupBox4.ResumeLayout(false);
-			this.groupBox4.PerformLayout();
 			this.groupBox3.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.ComboBox comboBoxFreeMovementKey;
-		private System.Windows.Forms.CheckBox checkBoxPrimaryReturnUnhindered;
-		private System.Windows.Forms.ComboBox comboBoxCursorMode;
-		private System.Windows.Forms.Label label15;
-		private System.Windows.Forms.CheckBox checkBoxControlUnhindersCursor;
-		private System.Windows.Forms.GroupBox groupBox4;
 		private System.Windows.Forms.Label label13;
 		private System.Windows.Forms.Label label11;
 		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.HScrollBar scrollBarSticky;
 		private System.Windows.Forms.GroupBox groupBox3;
 		private Library.HotKeys.HotKeyPanel hotKeyPanelCursorPrevScreen;
 		private Library.HotKeys.HotKeyPanel hotKeyPanelCursorNextScreen;
-		private Library.HotKeys.HotKeyPanel hotKeyPanelLockCursor;
-		private Library.HotKeys.HotKeyPanel hotKeyPanelStickyCursor;
-		private Library.HotKeys.HotKeyPanel hotKeyPanelFreeCursor;
-		private System.Windows.Forms.CheckBox checkBoxAllowButton;
-		private System.Windows.Forms.ComboBox comboBoxFreeMovementButton;
 		private Library.HotKeys.HotKeyPanel hotKeyPanelCursorToPrimaryScreen;
 
 	}
